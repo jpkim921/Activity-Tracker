c37a170775bc257cda5829f1ca876aaa3f81df3e
Jayon <jpkim921@gmail.com>
2018-06-17 14:47:01 -0400

    updated a few files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+
+class UsersController < ApplicationController
+
+  get '/user/signup' do
+    "TEST"
+    # erb :'/user/signup'
+  end
+
+  # post '/user/signup' do
+  #   params
+  # end
+
+
+end
--- config.ru
+++ config.ru
@@ -7,5 +7,5 @@ end
 
 use Rack::MethodOverride
 
-
+use UsersController
 run ApplicationController
