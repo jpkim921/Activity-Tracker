4ac5dfa3781da7c43b6e21713141fabccd718b12
Jayon <jpkim921@gmail.com>
2018-06-17 14:49:28 -0400

    added get sign up for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@
 class UsersController < ApplicationController
 
   get '/user/signup' do
-    "TEST"
-    # erb :'/user/signup'
+    # "TEST"
+    erb :'/user/signup'
   end
 
   # post '/user/signup' do
