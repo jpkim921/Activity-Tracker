e8cefe90d46ad5c4dfc616aa2cb2b8726e4a3858
Jayon <jpkim921@gmail.com>
2018-06-18 00:05:15 -0400

    activity-index to activity-new to activity-show

--- app/controller/activities_controller.rb
+++ app/controller/activities_controller.rb
@@ -1,5 +1,43 @@
 class ActivitiesController < ApplicationController
   get '/activities' do
-    erb:'/activity/activities'
+    if !logged_in?
+      redirect '/login'
+    else
+      @activities = Activity.all
+      erb :'/activity/activities'
+    end
   end
+
+  get '/activity/new' do
+    if !logged_in?
+      redirect '/login'
+    else
+      erb :'/activity/new'
+    end
+  end
+
+  post '/activity/new' do
+    # binding.pry
+    if !logged_in?
+      redirect '/login'
+    elsif params.empty?
+      redirect '/activity/new'
+    else
+      # category = Category.new(activity_type: params[:category])
+      params.delete_if { |k, v| v == "" || k == "category" }
+      @activity = Activity.create(params)
+      redirect "/activity/#{@activity.id}"
+    end
+  end
+
+  get '/activity/:id' do
+   @activity = Activity.find(params[:id])
+   if logged_in?
+     erb :"/activity/show_activity"
+   else
+     redirect '/login'
+   end
+  end
+
+
 end
--- app/controller/users_controller.rb
+++ app/controller/users_controller.rb
@@ -22,5 +22,7 @@ class UsersController < ApplicationController
     end
   end
 
+  
+
 
 end
